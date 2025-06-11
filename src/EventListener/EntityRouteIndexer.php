eacd118868df47df8f1d9025ed181ac5ec08922f
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2025-06-11 12:32:34 +0200

    feat: update the child routes when updating one

--- src/EventListener/EntityRouteIndexer.php
+++ src/EventListener/EntityRouteIndexer.php
@@ -6,6 +6,7 @@ namespace Adeliom\SyliusHappyCMSPlugin\EventListener;
 
 use Adeliom\SyliusHappyCMSPlugin\Factory\CMS\CmsRoutableInterface;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Event\PostPersistEventArgs;
 use Doctrine\ORM\Event\PostUpdateEventArgs;
 use Sylius\Resource\Model\TranslationInterface;
@@ -25,6 +26,7 @@ class EntityRouteIndexer
 
     public function __construct(
         protected ContentRepository $contentRepository,
+        protected EntityManagerInterface $entityManager,
     ) {
     }
 
@@ -64,6 +66,8 @@ class EntityRouteIndexer
             return;
         }
 
+        $oldRoutes = $this->getOldRoutes($entity);
+
         if ($entity->isOnline()) {
             $this->manageRoutes($entity, self::ROUTE_ONLINE);
         }
@@ -74,6 +78,8 @@ class EntityRouteIndexer
 
         $event->getObjectManager()->persist($entity);
         $event->getObjectManager()->flush();
+
+        $this->updateChildRoutes($entity, $oldRoutes);
     }
 
     private function manageRoutes(CmsRoutableInterface &$entity, string $routeNamePrefix = ''): void
@@ -109,5 +115,53 @@ class EntityRouteIndexer
             $route->setDefault(RouteObjectInterface::CONTENT_ID, $this->contentRepository->getContentId($entity));
             $entity->addRoute($route);
         }
+    }
+
+    private function getOldRoutes(CmsRoutableInterface|\Sylius\Resource\Model\TranslatableInterface $entity): array
+    {
+        $oldRoutes = [];
+        foreach ($entity->getTranslations() as $translation) {
+            foreach ([self::ROUTE_ONLINE, self::ROUTE_PREVIEW] as $routeNamePrefix) {
+                $prefix = $routeNamePrefix . $translation->getLocale();
+                $routeName = $prefix . '_' . $entity->getRouteUnikName();
+
+                // Route exists ?
+                $route = $entity->getRoutes()->filter(static fn (Route $route) => $route->getName() === $routeName)->first();
+                if ($route) {
+                    $oldRoutes[$prefix] = [
+                        'from' => $route->getStaticPrefix()
+                    ];
+                }
+            }
+        }
+
+        return $oldRoutes;
+    }
+
+    private function updateChildRoutes(CmsRoutableInterface|\Sylius\Resource\Model\TranslatableInterface $entity, array $oldRoutes): void
+    {
+        $connection = $this->entityManager->getConnection();
+        $connection->beginTransaction();
+        foreach ($entity->getTranslations() as $translation) {
+            foreach ([self::ROUTE_ONLINE, self::ROUTE_PREVIEW] as $routeNamePrefix) {
+                $prefix = $routeNamePrefix . $translation->getLocale();
+                $routeName = $prefix . '_' . $entity->getRouteUnikName();
+
+                // Route exists ?
+                $route = $entity->getRoutes()->filter(static fn (Route $route) => $route->getName() === $routeName)->first();
+                if ($route) {
+                    $oldRoutes[$prefix]['to'] = $route->getStaticPrefix();
+                    $isFromPreview = str_ends_with($oldRoutes[$prefix]['from'], '-preview') && $routeNamePrefix === self::ROUTE_PREVIEW;
+                    $isToPreview = str_ends_with($oldRoutes[$prefix]['to'], '-preview') && $routeNamePrefix === self::ROUTE_PREVIEW;
+                    $connection->executeQuery("UPDATE `orm_routes` SET staticPrefix = REPLACE(staticPrefix, ?, ?) WHERE staticPrefix LIKE ? AND name LIKE ?", [
+                        str_replace($isFromPreview ? '-preview' : '', '', $oldRoutes[$prefix]['from']),
+                        str_replace($isToPreview ? '-preview' : '', '', $oldRoutes[$prefix]['to']),
+                        str_replace($isFromPreview ? '-preview' : '', '', $oldRoutes[$prefix]['from'] . '%'),
+                        $prefix . '%',
+                    ]);
+                }
+            }
+        }
+        $connection->commit();
     }
 }
