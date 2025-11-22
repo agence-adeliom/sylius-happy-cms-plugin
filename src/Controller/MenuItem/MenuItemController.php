720f193d2bad866f762e8f46aedeea3146abbae9
Jérôme Engeln <jerome.engeln@adeliom.com>
2025-11-22 10:12:09 +0100

    phpstan
    Signed-off-by: Jérôme Engeln <jerome.engeln@adeliom.com>

--- src/Controller/MenuItem/MenuItemController.php
+++ src/Controller/MenuItem/MenuItemController.php
@@ -10,23 +10,15 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\RouterInterface;
-use Webmozart\Assert\Assert;
 
 class MenuItemController
 {
-    private ?MenuItemRepositoryInterface $menuItemRepository;
-
     public function __construct(
-        private EntityManagerInterface $entityManager,
         private RouterInterface $router,
     ) {
-        $repository = $this->entityManager->getRepository(MenuItemInterface::class);
-        if ($repository instanceof MenuItemRepositoryInterface) {
-            $this->menuItemRepository = $repository;
-        }
     }
+
 
     public function indexAction(Request $request): Response
     {
